a205c42ba8e10c3540534c69ad4de2117e82f48f
fluency03 <fluency.03@gmail.com>
2017-11-21 23:10:13 +0100

    add tests for append, init, dropWhile2

--- src/test/scala/com/fluency03/fpscala/datastructures/ListTest.scala
+++ src/test/scala/com/fluency03/fpscala/datastructures/ListTest.scala
@@ -4,6 +4,18 @@ import org.scalatest._
 
 class ListTest extends FlatSpec with Matchers {
 
+  "Nil" should "represent an empty List." in {
+    Nil should equal(List[Nothing]())
+  }
+
+  "Cons" should "represent a List with its head and tail." in {
+    val c = Cons(1, Nil)
+    c shouldBe a [List[_]]
+    c should equal(List(1))
+    c.head should equal(1)
+    c.tail should equal(Nil)
+  }
+
   "apply" should "construct a List." in {
     List() should equal(Nil)
     List(1) should equal(Cons(1, Nil))
@@ -56,10 +68,65 @@ class ListTest extends FlatSpec with Matchers {
     List.setHead(List(1, 2, 3), 2) should equal(List(2, 2, 3))
   }
 
+  "drop" should "drop the first n elements from a List." in {
+    List.drop(List(1), -1) should equal(List(1))
+    List.drop(List(1), 0) should equal(List(1))
 
+    List.drop(List(1, 2, 3), 1) should equal(List(2, 3))
+    List.drop(List(1, 2, 3), 2) should equal(List(3))
+    List.drop(List(1, 2, 3), 3) should equal(Nil)
+  }
 
+  "dropWhile" should "drop all element which are making the condition true from the begining of a List." in {
+    List.dropWhile[Int](Nil, _ > 0) should equal(Nil)
+    List.dropWhile[Int](List(1, -1), _ > 0) should equal(List(-1))
+    List.dropWhile[Int](List(1), _ > 0) should equal(Nil)
+    List.dropWhile[Int](List(-1), _ > 0) should equal(List(-1))
+    List.dropWhile[Int](List(-1, 2, 3), _ > 0) should equal(List(-1, 2, 3))
+    List.dropWhile[Int](List(1, -2, -3), _ > 0) should equal(List(-2, -3))
+    List.dropWhile[Int](List(-1, 2, -3), _ > 0) should equal(List(-1, 2, -3))
+    List.dropWhile[Int](List(1, -2, 3), _ > 0) should equal(List(-2, 3))
+    List.dropWhile[Int](List(1, 2, 3), _ > 0) should equal(Nil)
+  }
 
+  "dropWhile2" should "drop all element which are making the condition true from the beginning of a List." in {
+    List.dropWhile2[Int](Nil)(_ > 0) should equal(Nil)
+    List.dropWhile2[Int](List(1, -1))(_ > 0) should equal(List(-1))
+    List.dropWhile2[Int](List(1))(_ > 0) should equal(Nil)
+    List.dropWhile2[Int](List(-1))(_ > 0) should equal(List(-1))
+    List.dropWhile2[Int](List(-1, 2, 3))(_ > 0) should equal(List(-1, 2, 3))
+    List.dropWhile2[Int](List(1, -2, -3))(_ > 0) should equal(List(-2, -3))
+    List.dropWhile2[Int](List(-1, 2, -3))(_ > 0) should equal(List(-1, 2, -3))
+    List.dropWhile2[Int](List(1, -2, 3))(_ > 0) should equal(List(-2, 3))
+    List.dropWhile2[Int](List(1, 2, 3))(_ > 0) should equal(Nil)
+  }
 
+  "append" should "append one List at the end of another." in {
+    List.append(Nil, Nil) should equal(Nil)
+    List.append(Nil, List(1)) should equal(List(1))
+    List.append(List(1), Nil) should equal(List(1))
+    List.append(List(1, 2), List(3, 4)) should equal(List(1, 2, 3, 4))
+  }
+
+  "init" should "returns a List consisting of all but the last element of a List." in {
+    the [NoSuchElementException] thrownBy {
+      List.init(Nil)
+    } should have message "No initial element for an empty list."
+
+    List.init(List(1)) should equal(Nil)
+    List.init(List(1, 2)) should equal(List(1))
+    List.init(List(1, 2, 3)) should equal(List(1, 2))
+  }
+
+  "initTailRec" should "returns a List consisting of all but the last element of a List." in {
+    the [NoSuchElementException] thrownBy {
+      List.initTailRec(Nil)
+    } should have message "No initial element for an empty list."
+
+    List.initTailRec(List(1)) should equal(Nil)
+    List.initTailRec(List(1, 2)) should equal(List(1))
+    List.initTailRec(List(1, 2, 3)) should equal(List(1, 2))
+  }
 
 
 
